542599903303a27fe197f4b901a3081856436c34
Noor <yeminzaw04.com>
2025-11-26 10:41:07 -0800

    Sum all

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+/*
+Implement a function that takes 2 positive integers and returns the sum of every integer between (and including) them:
 
+```javascript
+sumAll(1, 4) // returns the sum of 1 + 2 + 3 + 4 which is 10
+*/
+const sumAll = function(start, end) {
+    if (!Number.isInteger(start) || !Number.isInteger(end)) return "ERROR";
+    if (start < 0 || end < 0) return "ERROR";
+    if (start > end) {
+        [start, end] = [end, start];
+    }
+    let sum = 0;
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
