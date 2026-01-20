ab727103c48e09eb3268e7fda0ab100297026600
Noor <yeminzaw04.com>
2026-01-20 12:25:13 -0800

    feat: add solution of calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,26 +1,28 @@
-const add = function() {
-	
-};
+const add = (a, b) => a + b;
 
-const subtract = function() {
-	
-};
+const subtract = (a, b) => a - b;
 
-const sum = function() {
-	
-};
+const sum = (nums) => (nums).reduce((sum, num) => sum + num, 0);
+console.log(sum([1, 2, 3]));
+// const multiply = (a, b) => a * b;
+const multiply = nums => nums.reduce((product, num) => product * num);
+console.log(multiply([1, 2, 3, 4]));
+const power = (a, b) => a ** b;
 
-const multiply = function() {
+const factorial = num => {
+  let result = 1;
+  if (num === 0) return result;
+  if (num === 1) return result;
 
-};
+  while(num > 1) {
+    result *= num;
+    num--;
+  }
 
-const power = function() {
-	
-};
+  return result;
+}
 
-const factorial = function() {
-	
-};
+console.log(factorial(4));
 
 // Do not edit below this line
 module.exports = {
