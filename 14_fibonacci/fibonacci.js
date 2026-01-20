5c94ce59f6f36415fad2345154062967157d320e
Noor <yeminzaw04.com>
2026-01-20 13:48:26 -0800

    feat: add fibonacci exercise solution

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
+const fibonacci = num => {
+    // Make sure the num is a number
+    num = Number(num);
 
-};
+    // Negative value or zero
+    if (num < 0) return "OOPS";
+    if (num === 0) return 0;
+
+    let first = 0;
+    let second = 1;
+
+    for (let i = 2; i <= num; i++) {
+        let current = first + second;
+        first = second;
+        second = current;
+    }
+
+    return second;
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
