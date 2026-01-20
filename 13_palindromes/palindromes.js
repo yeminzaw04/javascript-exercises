a0c0a4e4f503361333e5ba0f3e9be73db161b3a5
Noor <yeminzaw04.com>
2026-01-20 12:59:48 -0800

    feat: add palindrom exercise solution

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
-
+const palindromes = str => {
+    const alphabets = "abcdefghijklmnopqrstuvwxyz";
+    const nums = "1234567890";
+    const cleanStr = str
+    .toLowerCase()
+    .split("")
+    .filter(char => alphabets.includes(char.toLowerCase()) || nums.includes(char))
+    .join("");
+    const reversedStr = cleanStr.split("").reverse().join("");
+    return cleanStr === reversedStr;
 };
-
+console.log(palindromes('r3ace3car'))
 // Do not edit below this line
 module.exports = palindromes;
