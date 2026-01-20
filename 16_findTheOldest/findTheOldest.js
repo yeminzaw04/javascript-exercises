1bf72e3e2bcc47b47c3528d0a35e31deb8863fe6
Noor <yeminzaw04.com>
2026-01-20 14:15:59 -0800

    feat: add findTheOldest exercise solution

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,9 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = people => people.reduce((oldest, person) => {
+    let currentYear = new Date().getFullYear();
+    let ageOfOldest = oldest.yearOfDeath ? oldest.yearOfDeath - oldest.yearOfBirth : currentYear - oldest.yearOfBirth;
+    let ageOfPerson = person.yearOfDeath ? person.yearOfDeath - person.yearOfBirth : currentYear - person.yearOfDeath;
+    return ageOfOldest > ageOfPerson ? oldest : person;
+});
 
 // Do not edit below this line
 module.exports = findTheOldest;
